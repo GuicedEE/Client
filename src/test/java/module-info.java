53226c2aa2e10894cb19eff0d6bb8b9c2f4fe9c6
GedMarc <ged_marc@hotmail.com>
2026-03-01 05:06:21 +0200

    Refactor dependencies and add `maven-surefire-plugin` and module-info for test integration

---
+++ src/test/java/module-info.java
@@ -0,0 +1,6 @@
+module com.guicedee.inject.client.test {
+    requires com.guicedee.client;
+    requires org.junit.jupiter.api;
+
+    opens com.guicedee.client.test to org.junit.platform.commons;
+}
